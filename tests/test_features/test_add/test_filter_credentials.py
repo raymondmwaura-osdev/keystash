b4f84d0b3462fa2c3e2d20bbd00e83272ba4d764
raymondmwaura-osdev <raymondmwaura232@gmail.com>
2025-11-15 17:10:18 +0300

    tests: Add 'test_empty_parameters' and 'test_same_everything_different_password'.

--- tests/test_features/test_add/test_filter_credentials.py
+++ tests/test_features/test_add/test_filter_credentials.py
@@ -5,7 +5,7 @@ cred1 = {
     "password": "password1",
     "username": "username1",
     "email": "email1"
-    }
+}
 cred2 = {
     "service": "service2",
     "password": "password2",
@@ -28,6 +28,16 @@ class TestFilterCredentials:
 
         assert output_cred == []
 
+    def test_empty_parameters(self):
+        """
+        Validate that `add.filter_credentials` returns all credentials
+        when no filter field is provided.
+        """
+        input_cred = [cred1, cred2]
+        output_cred = add.filter_credentials(input_cred)
+
+        assert output_cred == input_cred
+
     def test_exact_duplicate(self):
         """
         Confirm that `add.filter_credentials` isolates and returns only
@@ -38,6 +48,19 @@ class TestFilterCredentials:
             input_cred,
             **cred1
         )
-        expected_output = [cred1]
+        assert output_cred == [cred1]
 
-        assert output_cred == expected_output
+    def test_same_everything_different_password(self):
+        """
+        Test that `add.filter_credentials` returns credentials matching all
+        specified fields except the password when the password argument is
+        not provided.
+        """
+        input_cred = [cred1, cred2]
+        output_cred = add.filter_credentials(
+            input_cred,
+            service=cred1["service"],
+            username=cred1["username"],
+            email=cred1["email"]
+        )
+        assert output_cred == [cred1]
