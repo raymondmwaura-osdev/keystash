ba8df502aed442532d37fdf7ee60b95b894310cc
raymondmwaura-osdev <raymondmwaura232@gmail.com>
2025-12-25 08:54:03 +0300

    Add 'main' function.

--- src/main.py
+++ src/main.py
@@ -3,64 +3,68 @@ from src.utils import constants, storage
 from getpass import getpass
 import argparse, sys, bcrypt
 
-# Build CLI.
-parser = argparse.ArgumentParser(prog="KeyStash")
-subparsers = parser.add_subparsers(dest="cmd")
+def main():
+    # Build CLI.
+    parser = argparse.ArgumentParser(prog="KeyStash")
+    subparsers = parser.add_subparsers(dest="cmd")
 
-build_cli_functions = [
-    add.build_cli,
-    search.build_cli,
-    passwd.build_cli,
-    remove.build_cli,
-    get.build_cli
-]
-for build_cli in build_cli_functions:
-    build_cli(subparsers)
+    build_cli_functions = [
+        add.build_cli,
+        search.build_cli,
+        passwd.build_cli,
+        remove.build_cli,
+        get.build_cli
+    ]
+    for build_cli in build_cli_functions:
+        build_cli(subparsers)
 
-cli_args = parser.parse_args()
+    cli_args = parser.parse_args()
 
-# Verify user identity.
-if constants.HASH.exists():
-    password_hash = constants.HASH.read_text().strip()
-    for _ in range(3):
-        password = getpass("Enter master password: ").strip()
-        
-        if bcrypt.checkpw(
-            password.encode("utf-8"),
-            password_hash.encode("utf-8")
-        ):
-            constants.MASTER_PASSWORD = password
-            break
+    # Verify user identity.
+    if constants.HASH.exists():
+        password_hash = constants.HASH.read_text().strip()
+        for _ in range(3):
+            password = getpass("Enter master password: ").strip()
+            
+            if bcrypt.checkpw(
+                password.encode("utf-8"),
+                password_hash.encode("utf-8")
+            ):
+                constants.MASTER_PASSWORD = password
+                break
 
-        print("Incorrect master password!")
+            print("Incorrect master password!")
 
-    else: sys.exit()
+        else: sys.exit()
 
-elif cli_args.cmd != "passwd": # Allow the user to use only 'passwd' when constants.HASH doesn't exist.
-    print("Master password not set.")
-    print("Use 'keystash passwd' to set the master password.")
-    sys.exit()
+    elif cli_args.cmd != "passwd": # Allow the user to use only 'passwd' when constants.HASH doesn't exist.
+        print("Master password not set.")
+        print("Use 'keystash passwd' to set the master password.")
+        sys.exit()
 
-# Features.
-if cli_args.cmd == "add":
-    add.add(
-        service=cli_args.service,
-        username=cli_args.username,
-        email=cli_args.email
-    )
+    # Features.
+    if cli_args.cmd == "add":
+        add.add(
+            service=cli_args.service,
+            username=cli_args.username,
+            email=cli_args.email
+        )
 
-elif cli_args.cmd == "search":
-    search.search(
-        service=cli_args.service,
-        username=cli_args.username,
-        email=cli_args.email
-    )
+    elif cli_args.cmd == "search":
+        search.search(
+            service=cli_args.service,
+            username=cli_args.username,
+            email=cli_args.email
+        )
 
-elif cli_args.cmd == "passwd":
-    passwd.passwd()
+    elif cli_args.cmd == "passwd":
+        passwd.passwd()
 
-elif cli_args.cmd == "remove":
-    remove.remove(int(cli_args.id))
+    elif cli_args.cmd == "remove":
+        remove.remove(int(cli_args.id))
 
-elif cli_args.cmd == "get":
-    get.get(int(cli_args.id))
+    elif cli_args.cmd == "get":
+        get.get(int(cli_args.id))
+
+if __name__ == "__main__":
+    main()
