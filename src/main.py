7b17c6e041ea1711071acad59a0eebd7bba0d3c3
raymondmwaura-osdev <raymondmwaura232@gmail.com>
2026-01-04 15:36:53 +0300

    Add function 'verify_identity'.

--- src/main.py
+++ src/main.py
@@ -1,10 +1,21 @@
+"""
+Interactive mode plans:
+    If cli_args.cmd == None, enter interactive mode.
+    If the user runs keystash with a specific command, run the command and exit.
+    If the user runs a specific command and uses the `-i/--interactive` flag, execute the command then enter interactive mode.
+    The `-i/--interactive` flag should only be on the main parser, not in every command's subparser.
+"""
 from src.features import add, search, passwd, remove, get
 from src.utils import constants, storage
 from getpass import getpass
 import argparse, sys, bcrypt
 
-def main():
-    # Build CLI.
+def build_cli():
+    """
+    Setup all CLI commands and options.
+
+    Return the top level parser got by `argparse.ArgumentParser()`.
+    """
     parser = argparse.ArgumentParser(prog="KeyStash")
     subparsers = parser.add_subparsers(dest="cmd")
 
@@ -18,52 +29,77 @@ def main():
     for build_cli in build_cli_functions:
         build_cli(subparsers)
 
-    cli_args = parser.parse_args()
+    return parser
 
-    # Verify user identity.
+def verify_identity(cmd: None | str) -> str:
+    """
+    Verify user identity by prompting for the master password.
+
+    Parameters:
+        cmd:
+            The cli command passed in by the user.
+
+    Return the password if the user is verified, exit otherwise.
+    Exit if the master password is not set and 'cmd' != "passwd".
+    """
     if constants.HASH.exists():
         password_hash = constants.HASH.read_text().strip()
         for _ in range(3):
             password = getpass("Enter master password: ").strip()
-            
+
             if bcrypt.checkpw(
                 password.encode("utf-8"),
                 password_hash.encode("utf-8")
             ):
-                constants.MASTER_PASSWORD = password
-                break
+                return password
 
             print("Incorrect master password!")
 
-        else: sys.exit()
+        sys.exit()
 
-    elif cli_args.cmd != "passwd": # Allow the user to use only 'passwd' when constants.HASH doesn't exist.
+    elif cmd != "passwd": # Allow the user to use only 'passwd' when constants.HASH doesn't exist.
         print("Master password not set.")
         print("Use 'keystash passwd' to set the master password.")
         sys.exit()
 
-    # Features.
-    if cli_args.cmd == "add":
+def main():
+    parser = build_cli()
+    cli_args = parser.parse_args()
+    constants.MASTER_PASSWORD = verify_identity(cli_args.cmd)
+
+    if cli_args.cmd == None:
+        # Call interactive mode function.
+        pass
+
+    else:
+        # Call a function that handles only one command.
+        handle_one_command(cli_args.cmd)
+
+def handle_one_command(cmd):
+    """
+    Run one command passed in from the user and exit.
+    """
+    if cmd == "add":
         add.add(
             service=cli_args.service,
             username=cli_args.username,
             email=cli_args.email
         )
 
-    elif cli_args.cmd == "search":
+    elif cmd == "search":
         search.search(
             service=cli_args.service,
             username=cli_args.username,
             email=cli_args.email
         )
 
-    elif cli_args.cmd == "passwd":
+    elif cmd == "passwd":
         passwd.passwd()
 
-    elif cli_args.cmd == "remove":
+    elif cmd == "remove":
         remove.remove(int(cli_args.id))
 
-    elif cli_args.cmd == "get":
+    elif cmd == "get":
         get.get(int(cli_args.id))
 
 if __name__ == "__main__":
