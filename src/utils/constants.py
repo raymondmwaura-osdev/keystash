3b7827180318c252d85cf6f4112385649c15d030
raymondmwaura-osdev <raymondmwaura232@gmail.com>
2026-01-08 17:54:26 +0300

    Add 'DATA_DIR' for testing.

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -2,6 +2,7 @@ import pathlib
 
 MASTER_PASSWORD = None	# Will contain the master password provided by the user.
 
+#DATA_DIR = pathlib.Path("/mnt/data/keystash/test_data") # Only for testing to avoid modifying data from an existing keystash installation.
 DATA_DIR = pathlib.Path().home() / ".local/share/keystash"
 VAULT = DATA_DIR / "vault"
 HASH = DATA_DIR / "hash"
